4729f793017b3b9c84345a8318414106753fb3dd
Clovis Shropshire <shropshireclovis@gmail.com>
2020-12-26 03:55:50 -0500

    Added Material UI

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import {Button, TextField } from '@material-ui/core'
 
 const TodoForm = ({input, setInput, todos, setTodos}) => {
+    const submitTodo = (e) => {
+        e.preventDefault();
+        setTodos([...todos, input])
+    }
     return (
         <div>
-            <form onSubmit={(e) => {
-                e.preventDefault();
-                setTodos([...todos, input])
-            }}>
-                <input type="text" value={input} onChange={e => setInput(e.target.value)} />
-                <button>Add Todo</button>
+            <form noValidate onSubmit={submitTodo}>
+                <TextField label="Enter ToDo Here..." type="text" value={input} onChange={e => setInput(e.target.value)} />
+                <Button color='primary' variant="contained" onClick={submitTodo}>Add Todo NOW</Button>
             </form>
         </div>
     )
